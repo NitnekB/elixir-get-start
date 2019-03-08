719712cf7aaffc0816ae99d099d58d88fd4eb91e
nitnekb <nitnekb@gmail.com>
2019-03-08 09:05:53 +0100

    feat: add missing delete function with tests

--- test/kv/bucket_test.exs
+++ test/kv/bucket_test.exs
@@ -12,4 +12,12 @@ defmodule KV.BucketTest do
     KV.Bucket.put(bucket, "milk", 3)
     assert KV.Bucket.get(bucket, "milk") == 3
   end
+
+  test "remove key and value from bucket", %{bucket: bucket} do
+    KV.Bucket.put(bucket, "test", 2)
+    assert KV.Bucket.get(bucket, "test") == 2
+
+    assert KV.Bucket.delete(bucket, "test") == 2
+    assert KV.Bucket.get(bucket, "test") == nil
+  end
 end
